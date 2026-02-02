d3e232209f8fd41d7826edbb01b2756e00e7d5fb
Ethan Weinbrecht <eweinbrecht@lcs.on.ca>
2026-02-02 15:27:22 -0500

    Initial Commit

---
+++ cool exploration thing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cool exploration thing
+//
+//  Created by Ethan Edmond Weinbrecht on 2026-02-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
