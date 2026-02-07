de1c5f3c59c3cc0607027adeaa196edf17b641dc
Ethan Weinbrecht <eweinbrecht@lcs.on.ca>
2026-02-07 13:50:49 -0500

    Completed first messsage layoiut using blocks.

---
+++ BlockLayout/BlockLayout/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  BlockLayout
+//
+//  Created by Ethan Edmond Weinbrecht on 2026-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+            Rectangle()
+            
+            HStack {
+                Group {
+                    Rectangle()
+                    Rectangle()
+                    Rectangle()
+                    Rectangle()
+                    
+                    .aspectRatio(1.0/1.0, contentMode:.fit )}
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
